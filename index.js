f76a0686a5a53b66069657cb5b26600c5c0d72a2
Lucas Barros <lucas.mr.barros@live.com>
2025-01-03 18:07:24 -0300

    html and css implemented

---
+++ index.js
@@ -0,0 +1,14 @@
+const display = document.getElementById("display")
+
+function appendToDisplay(sign){
+    
+    console.log(`the sign is ${sign}`);
+}
+
+function calculate(){
+    
+}
+
+function clear(){
+    display.value = '';
+}
